4007527dc22d1b8dad182f79370eade9bb080ef5
Steven Danna <steve@opscode.com>
2012-08-18 23:12:01 -0700

    Initial commit of openstack support.
    This commit provides basic support for running test-kitchen against
    an openstack installation.  To do this it
    
       * Provides a new `openstack` DSL for global openstack configuration
         values,
    
       * Adds an TestKitchen::Environment::Openstack class which maintains
         state information about openstack VMs and interacts with Fog,
    
       * Introduces an openstack runner that implements the core
         TestKitchen functionality, and
    
       * Alters CLI functions that assumed vagrant was the only runner.
    
    This runner depends on Fog to handle interactions with the Openstack API.

--- lib/test-kitchen/project/base.rb
+++ lib/test-kitchen/project/base.rb
@@ -77,7 +77,7 @@ module TestKitchen
       end
 
       def runner(arg=nil)
-        set_or_return(:runner, arg, :default => 'vagrant')
+        set_or_return(:runner, arg, nil)
       end
 
       def language(arg=nil)
